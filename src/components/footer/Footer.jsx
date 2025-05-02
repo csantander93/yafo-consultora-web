a6dbc866db3357e883ee3ab4216cd1e273d5e71b
csantander93 <casdesarrollo.2022@gmail.com>
2025-05-01 23:48:13 -0300

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -32,6 +32,7 @@ const Footer = () => {
               itemProp="sameAs"
             >
               <FaLinkedin />
+              <span className="sr-only">LinkedIn de Yafo Consultora</span>
             </a>
           </div>
         </div>
