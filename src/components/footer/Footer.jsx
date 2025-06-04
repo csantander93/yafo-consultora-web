f31c0382460a62875a96856989d564b2015842cf
csantander <casdesarrollo.2022@gmail.com>
2025-06-04 13:55:00 -0300

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-logo-section" itemScope itemType="https://schema.org/Organization">
           <a 
-            href="/" 
+            href="#" 
             target="_blank" 
             rel="noopener noreferrer" 
             aria-label="Visitar sitio de Yafo Consultora"
